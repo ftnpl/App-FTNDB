563e3ed1035024b53a641ea7b9155738a25b3907
RJ Clay <jame@rocasa.us>
2012-10-08 11:57:31 -0400

    Bump version to v0.37

--- lib/FTNDB.pm
+++ lib/FTNDB.pm
@@ -7,11 +7,11 @@ FTNDB - Administration of SQL databases for Fidonet/FTN processing.
 
 =head1 VERSION
 
-Version 0.36
+Version 0.37
 
 =cut
 
-our $VERSION = '0.36';
+our $VERSION = '0.37';
 
 =head1 DESCRIPTION
 
